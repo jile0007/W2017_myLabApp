4e23071ceaab1d53b34900f2fba03c61dda94d8d
emma_jil <jile0007@algonquinlive.com>
2017-01-16 17:54:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/emma_jil/w2017_mylabapp/StartActivity.java
@@ -0,0 +1,13 @@
+package com.example.emma_jil.w2017_mylabapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
